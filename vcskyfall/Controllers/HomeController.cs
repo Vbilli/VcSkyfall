6d6f6ab7024fccfbc28bdb7e75eb9a28ff8d8644
vbilli <liuhuibing163@sina.com>
2016-02-01 19:54:03 +0800

    adjust post formate and scroll down automatic ajax content function

--- vcskyfall/Controllers/HomeController.cs
+++ vcskyfall/Controllers/HomeController.cs
@@ -9,6 +9,8 @@ namespace vcskyfall.Controllers
 {
     public class HomeController : Controller
     {
+        public int Take = 3;
+        
         public ActionResult Index()
         {
             using (var db = new VcSkyfall())
@@ -66,6 +68,38 @@ namespace vcskyfall.Controllers
                 return View(query);
             }
 
+        }
+        public JsonResult AjaxMoreArcticle(int number)
+        {
+            using (var db = new VcSkyfall())
+            {
+                var jsonResult = new Dictionary<int, object[]>();
+                var PostQuery = (from p
+                            in db.Posts
+                             orderby p.TimeStamp
+                             descending
+                             select p)
+                             .Skip(number)
+                            .Take(Take)
+                            .ToArray();
+                var json = Array.ConvertAll(PostQuery, query => new
+                {
+                    id=query.id,
+                    Creater = query.Creater,
+                    Content = query.Content,
+                    PicturePath = query.PicturePath,
+                    TimeStamp = query.TimeStamp,
+                    Subject = query.Subject,
+                    Tittle = query.Tittle
+                });
+                //for (int i = 0; i < Take; i++)
+                //{
+                //    jsonResult.Add(i, json);
+                //}
+
+
+                return Json(json);
+            }
         }
     }
 }
---
+++ vcskyfall/Scripts/HomePage.js
@@ -0,0 +1,66 @@
+﻿$(function () {
+    $(window).scroll(function () {
+        //$(document).scrollTop() 获取垂直滚动的距离
+        //$(document).scrollLeft() 这是获取水平滚动条的距离
+        if ($(document).scrollTop() <= 0) {
+            //alert("滚动条已经到达顶部为0");
+        }
+
+
+        if ($(document).scrollTop() >= $(document).height() - $(window).height()) {
+            var Post = document.getElementsByClassName("post-preview");
+            var PostCount = Post.length;
+            GetMoreArticle(PostCount);
+            //alert("滚动条已经到达底部为" + $(document).scrollTop());
+        }
+    });
+})
+
+function GetMoreArticle(AlreadyLoadedNum) {
+    $.ajax({
+        url: '/Home/AjaxMoreArcticle',
+        type: 'post',
+        data: {"number": AlreadyLoadedNum },
+        success: function (ReturnedJson) {
+            var count = ReturnedJson.length;
+            if (count != 0) {
+                var CurrentArcticle = document.getElementsByClassName("post-preview");
+                var last = document.getElementsByClassName("pager")[0];
+                for (var i = 0; i < count ; i++) {
+                    var date = new Date();
+                    date.setDate(ReturnedJson[i].TimeStamp);
+                    var HtmlString = "<div class='post-preview'><a href='/Home/SamplePost/"
+                            + ReturnedJson[i].id
+                            + "'>" + "<h2 class='post-title'>"
+                            + ReturnedJson[i].Tittle
+                            + "</h2><h3 class='post-subtitle'>"
+                            + ReturnedJson[i].Subject
+                            + "</h3></a><p class='post-meta'>由 <a href='#'>"
+                            + ReturnedJson[i].Creater
+                            + "</a> 发布，于 "
+                            + date
+                            + "</p></div>"; // HTML string
+                    var div = document.createElement('div');
+                    div.innerHTML = HtmlString;
+                    var elements = div.childNodes;
+                    last.parentNode.insertBefore(div, last);
+                    //<div class='post-preview'>
+                    //    <a href='/Home/SamplePost/14'>
+                    //        <h2 class='post-title'>
+                    //            在这个 “一个 90 后可以决定让另一个 90 后手握 1 亿现金'的时代，80 后的我为什么不焦虑？
+
+                    //        </h2>
+                    //        <h3 class='post-subtitle'>
+                    //            36氪
+
+                    //        </h3>
+                    //    </a>
+                    //    <p class='post-meta'>由 <a href='#'>刘惠彬</a> 发布，于 1/25/2016 12:00:00 PM</p>
+                    //</div>
+                }
+            }
+        }
+        });
+
+
+}
