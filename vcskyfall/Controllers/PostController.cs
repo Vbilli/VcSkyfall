075430daebb2da2bc5a533bd7d815801d67e3278
vbilli <liuhuibing163@sina.com>
2016-02-23 18:37:25 +0800

    create post

--- vcskyfall/Controllers/PostController.cs
+++ vcskyfall/Controllers/PostController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using vcskyfall.Models;
 
 namespace vcskyfall.Controllers
 {
@@ -13,9 +14,46 @@ namespace vcskyfall.Controllers
         {
             return View();
         }
-        public ActionResult NewPost()
+        public ActionResult CreatePost()
         {
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ValidateInput(false)]
+        public ActionResult CreatePost(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new VcSkyfall())
+                {
+                    try
+                    {
+                        db.Posts.Add(new Post
+                        {
+                            Tittle = post.Tittle,
+                            TimeStamp = System.DateTime.Now,
+                            Creater = User.Identity.Name,
+                            Content = post.Content,
+                            Deleted = false,
+                            Subject = post.Subject,
+                            Level = 1
+                        });
+                        db.SaveChanges();
+                        return RedirectToAction("Index","Home");
+                    }
+                    catch (Exception e)
+                    {
+                        ModelState.AddModelError(string.Empty, e.Message);
+                        return View(post);
+                    }
+                }
+            }
+            else
+            {
+                return View(post);
+            }
+            
         }
     }
 }
---
+++ vcskyfall/Scripts/CreatePost.js
@@ -0,0 +1,44 @@
+﻿$(function () {
+    $('.jqte').jqte();
+    //var editor = document.getElementsByClassName("jqte_editor")[0];
+    //editor.addEventListener("change", function (event) {
+    //    event.preventDefault();
+    //    $('#copyText').html() = $('.jqte_editor').html();
+    //})
+    $(document).ready(function () {
+        var $editor = $('.jqte_editor')[0];
+        $editor.onchange = function () {
+            document.getElementById("Content").value = htmlEscape($('.jqte_editor').html());
+        }
+    });
+
+    //function CopyText(event) {
+
+    //    $('#copyText').html()=$('.jqte_editor').html()
+    //}
+})
+
+//var myVar = setInterval(myTimer, 1000);
+
+//function myTimer() {
+//    var d = new Date();
+//    //document.getElementById("copyText").innerHTML = d.toLocaleTimeString();
+//    document.getElementById("Content").innerHTML = $('.jqte_editor').html();
+//}
+
+function htmlEscape(str) {
+    return String(str)
+            .replace(/&/g, '&amp;')
+            .replace(/"/g, '&quot;')
+            .replace(/'/g, '&#39;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;');
+}
+function htmlUnescape(value) {
+    return String(value)
+        .replace(/&quot;/g, '"')
+        .replace(/&#39;/g, "'")
+        .replace(/&lt;/g, '<')
+        .replace(/&gt;/g, '>')
+        .replace(/&amp;/g, '&');
+}
