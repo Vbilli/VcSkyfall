53ae2f9d4fc2ade3ce4c05c4e562f2932d7e2a13
hliu <hliu@HLIU>
2018-03-13 13:46:03 +0800

    ddd

--- vcskyfall/Models/LevelType.cs
+++ vcskyfall/Models/LevelType.cs
@@ -4,11 +4,12 @@ using System.ComponentModel.DataAnnotations;
 
 namespace vcskyfall.Models
 {
-    public class LevelType
-    {
-        [Key]
-        [Required]
-        public int id { get; set; }
-        public string LevelName { get; set; }
-    }
+	public class LevelType
+	{
+		[Key]
+		[Required]
+		public int id { get; set; }
+		public string LevelName { get; set; }
+		public
+	}
 }
